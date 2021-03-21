598a3bad9f1bb39f5f88bccdba88340bc1d9c1f7
Geraldo Henrique Neto <geraldo.henriqueneto@gmail.com>
2021-03-21 18:07:32 -0300

    Iniciando os trabalhos

--- src/components/Catalogo.js
+++ src/components/Catalogo.js
@@ -14,7 +14,7 @@ const Catalogo = ({ livros }) => {
                         </li>
                     ))}
             </ol>
-            <h2>Categoria Programação</h2>
+            <h2>Categoria Programação - Testando o Github</h2>
             <ol>
                 {livros
                     .filter(livro => livro.categoria === "programacao")
